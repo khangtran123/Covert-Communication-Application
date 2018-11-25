ba1150bc013ec8391f2930843681596a6af42bce
khangtran123 <khangh.tran@hotmail.com>
2018-11-24 16:49:35 -0800

    Update client.py

--- client.py
+++ client.py
@@ -3,10 +3,7 @@
 import optparse
 import os
 import sys
-<<<<<<< HEAD
-=======
 import time
->>>>>>> 65cee74de58f2622c2b31996762e51a1d4c91a2e
 from packetutil import *
 from bkutil import *
 from multiprocessing import Process
